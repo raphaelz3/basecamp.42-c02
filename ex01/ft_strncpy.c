a9366a02efb6a3b11ee276b0b557d48f6b107736
rgoncalv <rgoncalv@student.42sp.org.br>
2021-04-16 12:21:16 +0000

    basecamp 42SP

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: rgoncalv <rgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 14:10:15 by rgoncalv          #+#    #+#             */
-/*   Updated: 2021/04/15 19:36:04 by rgoncalv         ###   ########.fr       */
+/*   Updated: 2021/04/16 12:19:54 by rgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,15 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	unsigned int count;
 
 	count = 0;
-	while (count < n && *src != '\0')
+	while (count < n && src[count] != '\0')
 	{
 		dest[count] = src[count];
 		count++;
 	}
 	while (count < n)
+	{
 		dest[count] = '\0';
+		count++;
+	}
 	return (dest);
 }
