f7611a956c51d7376661f68a289c9ba277e6fcee
rgoncalv <rgoncalv@student.42sp.org.br>
2021-04-17 11:46:03 +0000

    Basecamp 42SP

--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -6,20 +6,24 @@
 /*   By: rgoncalv <rgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 17:47:16 by rgoncalv          #+#    #+#             */
-/*   Updated: 2021/04/15 19:08:53 by rgoncalv         ###   ########.fr       */
+/*   Updated: 2021/04/17 11:43:21 by rgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_str_is_alpha(char *str)
 {
-	while (*str != '\0')
+	int count;
+
+	count = 0;
+	while (str[count] != '\0')
 	{
-		if (*str >= 65 && *str <= 90)
-			str++;
-		if (*str >= 97 && *str <= 122)
-			str++;
+		if (str[count] >= 'a' && str[count] <= 'z')
+			count++;
+		else if (str[count] >= 'A' && str[count] <= 'Z')
+			count++;
 		else
 			return (0);
 	}
 	return (1);
 }
+
